291ddc3a92320014ef3d3dc5c5e4c4b869820d17
dablu2019 <kdablu2019@gmail.com>
2023-04-04 21:31:57 +0530

    commit

--- src/main/java/com/dablu/consume/ConsumeRestApiApplication.java
+++ src/main/java/com/dablu/consume/ConsumeRestApiApplication.java
@@ -9,6 +9,8 @@ public class ConsumeRestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumeRestApiApplication.class, args);
 		System.out.println("Welcome to our new feature");
+		System.out.println("Welcome to our new feature");
+		System.out.println("Welcome to our new feature");
 	}
 
 }
